ea5701b3fae79665792398d29c884dbf4abcf867
Harley Rayner <harleymteden@gmail.com>
2019-01-18 19:01:55 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -2,7 +2,7 @@ class AuthorsController < ApplicationController
 
   def show
     # binding.pry
-    @author = Author.find_by(params[:id])
+    # @author = Author.find_by(params[:id])
   end
 
   def new
